dce68552e8eccc8c2c7a647fe03ee34d1465fda3
Do Duy Dong <duydong.do.sdet@gmail.com>
2025-01-03 21:45:22 +0700

    Variables and DataTypes

---
+++ src/tutorial/J_01_Variables_DataTypes.java
@@ -0,0 +1,22 @@
+package tutorial;
+
+public class J_01_Variables_DataTypes {
+
+	public static void main(String[] args) {
+		int iNum = 10;
+		System.out.println(iNum);
+
+		double dNum = 10.13;
+		System.out.println(dNum);
+
+		boolean value = true;
+		System.out.println(value);
+
+		char letter = 'D';
+		System.out.println(letter);
+
+		String name = "Dong";
+		System.out.println(name);
+	}
+
+}
