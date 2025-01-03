8799edb5081ae2b5c834f22ec7f44a3769103796
Do Duy Dong <duydong.do.sdet@gmail.com>
2025-01-03 22:20:20 +0700

    Operators

---
+++ src/tutorial/J_02_Operators.java
@@ -0,0 +1,16 @@
+package tutorial;
+
+public class J_02_Operators {
+
+	public static void main(String[] args) {
+		int a = 13;
+		int b = 10;
+
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+	}
+
+}
